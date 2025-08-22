2af6722bdeb0e9ed30a0b06d532f5e8e4f4ed094
Marcos Lopes <marcos.informatica@crecisp.gov.br>
2025-08-22 10:38:16 -0300

    added line space

--- MedVoll/MedVoll.Web/Areas/Identity/Pages/Account/Login.cshtml.cs
+++ MedVoll/MedVoll.Web/Areas/Identity/Pages/Account/Login.cshtml.cs
@@ -119,10 +119,12 @@ namespace MedVoll.Web.Areas.Identity.Pages.Account
                     _logger.LogInformation("User logged in.");
                     return LocalRedirect(returnUrl);
                 }
+
                 if (result.RequiresTwoFactor)
                 {
                     return RedirectToPage("./LoginWith2fa", new { ReturnUrl = returnUrl, RememberMe = Input.RememberMe });
                 }
+
                 if (result.IsLockedOut)
                 {
                     _logger.LogWarning("User account locked out.");
